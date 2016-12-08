ade4e2d3675a8341c15f64d3c603b5ced82475d1
Diego Damato <diego.damato@m2mrj.local>
2016-12-08 10:14:42 -0200

    Consulta motorista

--- app/controladores/motorista.js
+++ app/controladores/motorista.js
@@ -1,11 +1,71 @@
-module.exports = function(){
+module.exports = function(app){
 
     let controlador = {};
 
-    controlador.obter = (req,res) =>{
+    controlador.obter = (req,res) => {
 
-        res.end();
-    }
+        let connection = new app.database.connectionFactory();
+        let motoristaDAO = new app.database.motoristaDAO(connection);
+
+        var cpf = req.query.cpf;
+        var dataAtualizacao = req.query.dataAtualizacao;
+
+        //var cpf = "1381887376as";
+        //var dataAtualizacao = "w";
+        var cliente = 209;
+       
+        if (cpf && dataAtualizacao){
+            motoristaDAO.obter(cliente, cpf, dataAtualizacao)
+                    .then( motorista => {
+                        if (!motorista.length){
+                            res.sendStatus(204);
+                        }else{
+                            res.json(motorista);
+                        }
+
+                    })
+                    .catch(erro => res.sendStatus(500));
+        }
+        
+        if (!cpf && !dataAtualizacao){
+            motoristaDAO.obterTodos(cliente)
+                    .then( motorista => {
+                        if(!motorista.length){
+                            res.sendStatus(204);
+                        }else{
+                            res.json(motorista);
+                        }
+                    })
+                    .catch(erro => res.sendStatus(500));
+        }
+
+        if (cpf && !dataAtualizacao){
+            motoristaDAO.obterPorCpf(cliente, cpf)
+                    .then( motorista => {
+                        if(!motorista.length){
+                            res.sendStatus(204);
+                        }else{
+                            res.json(motorista);
+                        }
+                    })
+                    .catch(erro => res.sendStatus(500));
+        }
+        
+        if (dataAtualizacao && !cpf){
+            motoristaDAO.obterPorDataAtualizacao(cliente, dataAtualizacao)
+                    .then( motorista => {
+                        if(!motorista.length){
+                            res.sendStatus(204);
+                        }else{
+                            res.json(motorista);
+                        }
+                    })
+                    .catch(erro => res.sendStatus(500));
+        }
+
+        connection.end();
+}
 
     return controlador;
 }
+
