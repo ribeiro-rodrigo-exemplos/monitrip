d7ee72614ba0483b0d53a4e7d9c0c3e046d57bce
Rodrigo Santos <rodrigo.santos@m2msolutions.com.br>
2017-01-09 15:59:35 -0200

    implementando o cache das rotas principais

--- app/rota/motoristaRoutes.js
+++ app/rota/motoristaRoutes.js
@@ -1,6 +1,9 @@
+const routeCache = require('route-cache');
+const cacheConfig = require('../bootstrap/config-bootstrap')()['cache'];
+
 module.exports = function(app){
     
     let controlador = app.beans.factory.motoristaController;
 
-    app.get('/api/v1/motoristas',controlador.obter.bind(controlador));
+    app.get('/api/v1/motoristas',routeCache.cacheSeconds(cacheConfig.motorista.seconds),controlador.obter.bind(controlador));
 }
