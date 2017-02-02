8dc932be27f0ce5f3b41e6ce5bae8c4dc49c9d5b
alexandreolavio <alexandre.olavio@m2msolutions.com.br>
2017-02-02 17:04:13 -0200

    no message

--- app/util/dateUtil.js
+++ app/util/dateUtil.js
@@ -1,16 +1,17 @@
 let moment = require('moment');
+let momentTimezone = require('moment-timezone');
 
 const formato = {
-    DATA_DMA_BARRA: 'dd/MM/yyyy',
-    DATA_DMA_TRACO: 'dd-MM-yyyy',
-    DATA_AMD_BARRA: 'yyyy/MM/dd',
-    DATA_AMD_TRACO: 'yyyy-MM-dd',
-    DATAHORA_DMA_BARRA: 'dd/MM/yyyy hh:mm:ss',
-    DATAHORA_DMA_TRACO: 'dd-MM-yyyy hh:mm:ss',
-    DATAHORA_AMD_BARRA: 'yyyy/MM/dd hh:mm:ss',
-    DATAHORA_AMD_TRACO: 'yyyy-MM-dd hh:mm:ss',
-    DATA_COMPLETA_MDA_BARRA: 'dd/MM/yyyy hh:mm:ss:sss',
-    DATA_COMPLETA_AMD_BARRA: 'yyyy/MM/dd hh:mm:ss:sss',
+    DATA_DMA_BARRA: 'DD/MM/YYYY',
+    DATA_DMA_TRACO: 'DD-MM-YYYY',
+    DATA_AMD_BARRA: 'YYYY/MM/DD',
+    DATA_AMD_TRACO: 'YYYY-MM-DD',
+    DATAHORA_DMA_BARRA: 'DD/MM/YYYY hh:mm:ss',
+    DATAHORA_DMA_TRACO: 'DD-MM-YYYY hh:mm:ss',
+    DATAHORA_AMD_BARRA: 'YYYY/MM/DD hh:mm:ss',
+    DATAHORA_AMD_TRACO: 'YYYY-MM-DD hh:mm:ss',
+    DATA_COMPLETA_MDA_BARRA: 'DD/MM/YYYY hh:mm:ss:sss',
+    DATA_COMPLETA_AMD_BARRA: 'YYYY/MM/DD hh:mm:ss:sss',
     HORA_MIN: 'hh:mm:ss',
     HORA_MIN_SEG: 'hh:mm:ss',
     HORA_COMPLETA: 'hh:mm:ss:sss'
@@ -23,12 +24,13 @@ const tipoRetorno = {
 
 class DateUtil{
     constructor(){
-        this._meses = new Array("Janeiro", "Fevereiro", "Marco", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
+        this._formato = formato;
+        this._tipoRetorno = tipoRetorno;
     }
 
-    validar(data){
+    validar(data, formato){
         try{                       
-            return moment(data, "YYYY-MM-DD", true).isValid();           
+            return moment(data, formato, true).isValid();           
         }
         catch(e){
             console.log(e);
@@ -41,5 +43,27 @@ class DateUtil{
         if(!format) format = formato.DATAHORA_AMD_BARRA;
         if(!timezone) timezone = 'America/Sao_Paulo';
         if(!tipo) tipo = tipoRetorno.STRING;
+
+        return this.aplicaTimeZone(dataHora, format, timezone, tipo);
+    }
+
+    aplicaTimeZone(dataHora, format, timezone, tipo){
+        let dh = momentTimezone.tz(dataHora, timezone);
+
+        return (tipo === tipoRetorno.STRING) ? dh.format(format) : dh.toDate();
+    }
+
+    get formato(){
+        return this._formato;
+    }
+
+    get tipoRetorno(){
+        return this._tipoRetorno;
+    }
+
+    getMesVigente(data){
+        return moment.months()[moment(data).format('M') -1];
     }
 }
+
+module.exports = () => DateUtil;
