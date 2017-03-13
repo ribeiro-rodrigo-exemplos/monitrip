15927d08381087865b1b7bad6fd72144b20d0ac9
Nataniel Souza <nataniel.souza@m2msolutions.com.br>
2017-03-13 10:41:42 -0300

    Funcionalidade #2241 - Inserção do campo fl_situacao na tabela de funcionários.

--- app/repositorio/motoristaRepository.js
+++ app/repositorio/motoristaRepository.js
@@ -18,7 +18,7 @@ module.exports = () =>
                 criteria["nm_cpf"] = cpf;
 
             return new Promise((resolve,reject) =>{
-                this._Motorista.findAll({where:criteria,attributes:['nm_cpf','nm_nomeFuncionario','fl_ativo']})
+                this._Motorista.findAll({where:criteria,attributes:['nm_cpf','nm_nomeFuncionario','fl_ativo', 'fl_situacao']})
                                 .then(result => resolve(result.length ? result : null))
                                 .catch(erro => reject(erro));
             });  
