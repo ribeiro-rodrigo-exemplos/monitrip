a1eaed895517e1414b0cee9bd1d4170a6cc8d6f6
Rodrigo Ribeiro <rodrigo.santos@m2msolutions.com.br>
2017-08-17 17:42:28 -0300

    alterando amqpUtil para trabalhar com apenas uma conexao

--- app/util/amqpUtil.js
+++ app/util/amqpUtil.js
@@ -9,25 +9,47 @@ module.exports = () =>
             
             logger.info(`AmqpUtil - enviarMensagem - mensagem: ${mensagem},queue - ${queue}, queueConfig - ${queueConfig}`);
 
+            if(this._connection)
+                this._enviarParaFila(mensagem,queue,queueConfig,mensagemConfig);
+            else
+                return this._criarConexao()
+                    .then(() => this._enviarParaFila(mensagem,queue,queueConfig,mensagemConfig))
+        }
+
+        _criarConexao(){
             return new Promise((resolve, reject) => {
                 amqp.connect(amqpConfig.url, (erro, connection) => {
 
                     if (erro) {
-                        reject(erro);
+                        reject(new Error(erro.message));
                         return;
                     }
 
                     connection.createChannel((err, channel) => {
 
-                        if (err)
+                        if (err){
                             reject(err);
-
-                        channel.assertQueue(queue,queueConfig);
-                        channel.sendToQueue(queue, Buffer.from(JSON.stringify(mensagem)),mensagemConfig,(err,ok) => connection.close());
+                            return;
+                        }
+                            
+                        this._channel = channel;
+                        this._connection = connection;  
 
                         resolve();
                     });
                 });
             });
+        }
+
+        _enviarParaFila(mensagem,queue,queueConfig,mensagemConfig){
+            try{
+                this._channel.assertQueue(queue,queueConfig);
+                this._channel.sendToQueue(queue, Buffer.from(JSON.stringify(mensagem)),mensagemConfig);
+            }
+            catch(e){
+                this._connection = null; 
+                this._channel = null;
+                throw new Error('A conexão com o Rabbitmq foi encerrada'); 
+            }
         }
     }
