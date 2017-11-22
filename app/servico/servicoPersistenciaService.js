bd1e32a24524e44767c5708dc8e3e0c6995f9bfa
diego <diego.damato>
2017-11-22 16:23:27 -0200

    Inserindo data de insercao do log

--- app/servico/servicoPersistenciaService.js
+++ app/servico/servicoPersistenciaService.js
@@ -2,9 +2,10 @@ const safira = require('safira');
 let amqpConfig =  safira.bean('config').amqp
 
 class ServicoPersistenciaService{
-    constructor(amqpUtil,logDTO){
+    constructor(amqpUtil,logDTO,dateUtil){
         this._amqpUtil = amqpUtil;
         this._logDTO = logDTO;
+        this._dateUtil = dateUtil;
         this._queue = amqpConfig['servico-persistencia'];
         this._rabbitConfig = {
             durable:true,
@@ -17,7 +18,7 @@ class ServicoPersistenciaService{
     salvarLog(log, infoCliente){
         
         log.appVersion = infoCliente.appVersion
-        
+        log.dataCriacao = this._dateUtil.aplicaTimeZoneEmUTC(new Date(),infoCliente.gmt);
         if(log.idLog < 250)
             return this._amqpUtil
                         .enviarMensagem(this._logDTO.toDTO('logsMonitrip', 'insert', log),
