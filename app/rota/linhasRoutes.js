28163e2e10d303798690e9669be68865ffe8481f
diego <diego.damato>
2017-12-07 14:22:24 -0200

    Ajuste na rota de linhas

--- app/rota/linhasRoutes.js
+++ app/rota/linhasRoutes.js
@@ -3,5 +3,5 @@ const safira = require('safira');
 let controlador = safira.bean('linhaController');
 let app = safira.bean('app');
 
-app.route('/api/v1/clientes/linhas')
+app.route('/api/v1/linhas')
     .get(controlador.obterLinhas.bind(controlador));
