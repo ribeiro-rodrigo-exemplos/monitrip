1059006075907d2b05fd332ef560d5749930598a
Alexandre Batista <alexandre.batista@M2MRJ.LOCAL>
2018-07-25 16:04:03 -0300

    Flag Cancelamento

--- app/repositorio/bilheteRepository.js
+++ app/repositorio/bilheteRepository.js
@@ -67,6 +67,7 @@ class BilheteRepository {
                         "clienteId": clienteId,
                         $and: [{"dataHoraEvento": {$gte: dataInicio}},
                             {"dataHoraEvento": {$lte: dataFim}}]
+                            , $and: [ { "flgCancelamento": { $ne: "false" } } ]
                     };
 
         return this._prepareResult(criteria, {"_class": 0, "_id": 0});
